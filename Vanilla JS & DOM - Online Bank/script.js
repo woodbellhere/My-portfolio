7c2a4fd895ed8066fbb5650d34f159cfedea2bb3
WoodBell wong <wangjd28@mail3.sysu.edu.cn>
2023-10-31 14:49:21 +0800

    minor change to online bank

--- Vanilla JS & DOM - Online Bank/script.js
+++ Vanilla JS & DOM - Online Bank/script.js
@@ -1,50 +1,50 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnsCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-const nav = document.querySelector('.nav');
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnsCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-btnsCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnsCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
-btnScrollTo.addEventListener('click', function (e) {
+btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
+  console.log("Current scroll (X/Y)", window.scrollX, window.scrollY);
 
   console.log(
-    'height/width viewport',
+    "height/width viewport",
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
@@ -62,7 +62,7 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
   // 以及简便方法
   section1.scrollIntoView({
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 });
 
@@ -78,54 +78,54 @@ btnScrollTo.addEventListener('click', function (e) {
 //   });
 // });
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+document.querySelector(".nav__links").addEventListener("click", function (e) {
   // console.log(e.target);
   e.preventDefault();
   // 匹配策略
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
     // 这里就相当于一次性全部注册了
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
 
 // tabs.forEach(t => addEventListener('click', () => console.log('TAB')));
 
-tabsContainer.addEventListener('click', function (e) {
+tabsContainer.addEventListener("click", function (e) {
   // 事件冒泡传上来的事件被触发后又选中目标元素
-  const clicked = e.target.closest('.operations__tab');
+  const clicked = e.target.closest(".operations__tab");
   console.log(clicked);
 
   // 哨兵/守卫语句
   if (!clicked) return;
 
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
 
   // activate tab
-  clicked.classList.add('operations__tab--active');
+  clicked.classList.add("operations__tab--active");
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('operations__content--active');
+    .classList.add("operations__content--active");
 });
 
 //
 // menu fate animation
 const handlerHover = function (e) {
   // console.log(this, e.currentTarget);
-  if (e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains("nav__link")) {
     const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
 
-    siblings.forEach(el => {
+    siblings.forEach((el) => {
       if (el !== link) el.style.opacity = this;
     });
     logo.style.opacity = this;
   }
 };
-nav.addEventListener('mouseover', handlerHover.bind(0.5));
-nav.addEventListener('mouseout', handlerHover.bind(1));
+nav.addEventListener("mouseover", handlerHover.bind(0.5));
+nav.addEventListener("mouseout", handlerHover.bind(1));
 
 // sticky navigation
 // const initialCoords = section1.getBoundingClientRect();
@@ -150,33 +150,33 @@ nav.addEventListener('mouseout', handlerHover.bind(1));
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(section1);
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
   // console.log(entry);
-  if (!entry.isIntersecting) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
 };
 // 这里是header离开视线，而不是第二部分进入视线
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: '-90px',
+  rootMargin: "-90px",
 });
 
 headerObserver.observe(header);
 
 //
 // reveal sections
-const allSection = document.querySelectorAll('.section');
+const allSection = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
   // console.log(entry);
   if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
+  entry.target.classList.remove("section--hidden");
   observer.unobserve(entry.target);
 };
 
@@ -187,12 +187,12 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSection.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  section.classList.add("section--hidden");
 });
 
 //
 // lazy loading images
-const imgTargets = document.querySelectorAll('img[data-src]');
+const imgTargets = document.querySelectorAll("img[data-src]");
 const loadImg = function (entries, observe) {
   const [entry] = entries;
   // console.log(entry);
@@ -201,9 +201,9 @@ const loadImg = function (entries, observe) {
   // replace data-src with src
   entry.target.src = entry.target.dataset.src;
   // 图片替换又有load事件
-  entry.target.classList.remove('lazy-img');
-  entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img');
+  entry.target.classList.remove("lazy-img");
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
   });
   observe.unobserve(entry.target);
 };
@@ -211,17 +211,17 @@ const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
   // 都可以，懒加载就是为了你要看才换
-  rootMargin: '100px',
+  rootMargin: "100px",
 });
-imgTargets.forEach(img => imgObserver.observe(img));
+imgTargets.forEach((img) => imgObserver.observe(img));
 
 //
 // carousel slider
 const slider = function () {
-  const slides = document.querySelectorAll('.slide');
-  const btnLeft = document.querySelector('.slider__btn--left');
-  const btnRight = document.querySelector('.slider__btn--right');
-  const dotContainer = document.querySelector('.dots');
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
 
   let curSlide = 0;
   const maxSlide = slides.length;
@@ -230,7 +230,7 @@ const slider = function () {
   const createDots = function () {
     slides.forEach((_, i) => {
       dotContainer.insertAdjacentHTML(
-        'beforeend',
+        "beforeend",
         `<button class='dots__dot' data-slide='${i}'></button>`
       );
     });
@@ -240,12 +240,12 @@ const slider = function () {
   // 选中点时加上选中样式
   const activateDot = function (slide) {
     document
-      .querySelectorAll('.dots__dot')
-      .forEach(dot => dot.classList.remove('dots__dot--active'));
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
 
     document
       .querySelector(`.dots__dot[data-slide="${slide}"]`)
-      .classList.add('dots__dot--active');
+      .classList.add("dots__dot--active");
   };
   activateDot(0);
 
@@ -286,17 +286,17 @@ const slider = function () {
   init();
   //
   // 事件处理
-  btnRight.addEventListener('click', nextSlide);
-  btnLeft.addEventListener('click', prevSlide);
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
 
-  document.addEventListener('keydown', function (e) {
+  document.addEventListener("keydown", function (e) {
     // console.log(e);
-    if (e.key === 'ArrowLeft') prevSlide();
-    e.key === 'ArrowRight' && nextSlide();
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
   });
 
-  dotContainer.addEventListener('click', function (e) {
-    if (e.target.classList.contains('dots__dot')) {
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
       // 在圆点dataset中预存图片索引数字
       // 点击时动态读取索引数字，并跳转圆点和图片
       const { slide } = e.target.dataset;
@@ -307,16 +307,10 @@ const slider = function () {
 };
 slider();
 
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built', e);
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree built", e);
 });
 
-window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e);
+window.addEventListener("load", function (e) {
+  console.log("Page fully loaded", e);
 });
-
-// window.addEventListener('beforeunload', function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   e.returnValue = '';
-// });
